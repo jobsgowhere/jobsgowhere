9e62bbaef441db489860e559f9526f4d7d2c42fb
Sheldon Cheng <sheldonscyd@gmail.com>
2020-04-04 01:02:23 +0800

    Initial Footer links and styling

--- ui/src/components/Footer.tsx
+++ ui/src/components/Footer.tsx
@@ -1,17 +1,58 @@
 import * as React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Container = styled.div`
   grid-area: footer;
+  color: var(--color-grey-300);
+
+  ul {
+    display: flex;
+    justify-content: center;
+    margin-top: 2rem;
+    list-style: none;
+    font-size: 0.875rem;
+  }
+  li:not(:last-child):after {
+    content: "·";
+    display: inline-block;
+    margin: 0 0.3rem;
+  }
+  a {
+    color: currentColor;
+    &:hover {
+      color: var(--color-blue);
+    }
+  }
 `;
 
-type FooterProps = {
-};
+type FooterProps = {};
 
 function Footer(props: FooterProps) {
   return (
     <Container>
-      Footer goes here…
+      <ul>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Privacy</a>
+        </li>
+        <li>
+          <a href="#">Terms</a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/jobsgowhere/jobsgowhere"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Github Repo
+          </a>
+        </li>
+        <li>
+          <a href="#">Contact Us</a>
+        </li>
+      </ul>
     </Container>
   );
 }
