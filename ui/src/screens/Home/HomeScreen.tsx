806610e10a3e11724420aa5b909b56883a4af6e2
Sheldon Cheng <sheldonscyd@gmail.com>
2020-04-05 18:09:09 +0800

    Add Button component

--- ui/src/screens/Home/HomeScreen.tsx
+++ ui/src/screens/Home/HomeScreen.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-import Main from '../../components/Main';
+import Button from "../../components/Button";
+import Main from "../../components/Main";
 
 const Container = styled(Main)`
   display: flex;
@@ -13,7 +14,27 @@ type HomeScreenProps = {};
 function HomeScreen(props: HomeScreenProps) {
   return (
     <Container>
-      <Link to="/posts">Go to job board…</Link>
+      <div>
+        <Link to="/posts">
+          <Button primary>Go to jobs board</Button>
+        </Link>
+      </div>
+
+      <div>
+        <h2>Buttons</h2>
+        <p>
+          <Button>Default Button</Button>
+        </p>
+        <p>
+          <Button primary>Primary Button</Button>
+        </p>
+        <p>
+          <Button secondary>Secondary Button</Button>
+        </p>
+        <p>
+          <Button active>Active Button</Button>
+        </p>
+      </div>
     </Container>
   );
 }
