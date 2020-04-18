616bd3e8cf69e407f3e6bc359d0268a1660b3099
Kajal Sinha <kajalsinha@gmail.com>
2020-04-18 17:26:43 +0800

    add tables and sqlboiler models

--- data/migrations/0001_create_initial_tables.down.sql
+++ data/migrations/0001_create_initial_tables.down.sql
@@ -1,1 +1,8 @@
 -- Add your rollback scripts here
+DROP TABLE IF EXISTS job_provider;
+DROP TABLE IF EXISTS job_seeker;
+DROP TABLE IF EXISTS job_skill_map;
+DROP TABLE IF EXISTS related_skills;
+DROP TABLE IF EXISTS skill;
+DROP TABLE IF EXISTS job;
+DROP TABLE IF EXISTS person;
