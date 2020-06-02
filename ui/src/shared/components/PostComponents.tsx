713a5ee38a202a9bcd19445f20955076d44d0256
Sheldon Cheng <sheldonscyd@gmail.com>
2020-06-03 02:54:25 +0800

    Update Post/PostDetail styles, separate file for Post components

---
+++ ui/src/shared/components/PostComponents.tsx
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+export const ContentContainer = styled.div`
+  flex: auto;
+  display: flex;
+  flex-direction: row;
+  padding: 1rem 1.5rem;
+  padding-left: 0.75rem;
+`;
+
+export const Avatar = styled.div`
+  flex: 0 0 auto;
+  margin-right: 1.375rem;
+`;
+
+export const Info = styled.div`
+  flex: auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const InfoHeader = styled.div`
+  display: flex;
+  margin-bottom: 0.5rem;
+`;
+
+export const Actions = styled.div`
+  display: flex;
+  margin-left: auto;
+  margin-right: -0.5rem;
+`;
+
+export const Title = styled.h2`
+  margin: 0 0 1rem;
+  font-size: 1.125rem;
+  line-height: 1.4;
+  color: var(--color-darkblue);
+  font-weight: 400;
+`;
+
+export const Name = styled.h3`
+  font-size: 1rem;
+  margin: 0.25rem 0;
+  color: var(--color-darkblue);
+`;
+
+export const Headline = styled.h4`
+  margin: 0;
+  font-size: 0.875rem;
+  font-weight: 400;
+  color: var(--color-grey-300);
+`;
+
+export const Description = styled.div`
+  color: var(--color-grey-300);
+  line-height: 1.4;
+`;
+
+export const Timestamp = styled.div`
+  margin-top: auto;
+  font-size: 0.875rem;
+  color: var(--color-grey-300);
+`;
+
+export const AvatarImage = styled.img`
+  width: 64px;
+  height: 64px;
+  border-radius: 32px;
+`;
--- ui/src/shared/components/PostDetail.tsx
+++ ui/src/shared/components/PostDetail.tsx
@@ -2,11 +2,29 @@ import * as React from "react";
 import styled from "styled-components";
 
 import { PostInterface } from "../../types";
+import Button from "../../components/Button";
+import FavouriteButton from "../../components/FavouriteButton";
+import {
+  ContentContainer,
+  Avatar,
+  AvatarImage,
+  Info,
+  InfoHeader,
+  Name,
+  Headline,
+  Actions,
+  Title,
+  Description,
+} from "./PostComponents";
 
 const Container = styled.div`
   background-color: white;
   border-radius: 0.875rem;
-  padding: 1.5rem;
+  padding-left: 0.75rem;
+`;
+
+const ButtonContainer = styled.div`
+  padding: 1rem 1.5rem 1.75rem 0.75rem;
 `;
 
 type PostDetailProps = {
@@ -14,11 +32,36 @@ type PostDetailProps = {
 };
 const PostDetail: React.FC<PostDetailProps> = function (props) {
   const { data } = props;
+  const { created_by: user } = data;
   return (
     <Container>
-      {data.id}
-      <h2>{data.title}</h2>
-      <div>{data.description}</div>
+      <ContentContainer>
+        <Avatar>
+          <AvatarImage src={user.avatar_url} />
+        </Avatar>
+        <Info>
+          <InfoHeader>
+            <div>
+              <Name>
+                {user.first_name} {user.last_name}
+              </Name>
+              <Headline>
+                {user.job_title} at {user.company}
+              </Headline>
+            </div>
+            <Actions>
+              <FavouriteButton active={data.favourite} />
+            </Actions>
+          </InfoHeader>
+          <Title>{data.title}</Title>
+          <Description>{data.description}</Description>
+        </Info>
+      </ContentContainer>
+      <ButtonContainer>
+        <Button fullWidth primary>
+          Connect with {user.first_name}
+        </Button>
+      </ButtonContainer>
     </Container>
   );
 };
