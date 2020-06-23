0b67206ca46dbf076cc3c987c1bc40f86b1f2eed
Sheldon Cheng <sheldonscyd@gmail.com>
2020-06-24 00:15:47 +0800

    Add infinite scroll pagination to talents

--- ui/src/screens/Talents/TalentsScreen.tsx
+++ ui/src/screens/Talents/TalentsScreen.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import styled from "styled-components";
+import axios from "axios";
 
 import { Main } from "../../components/Main";
 import Search from "../../shared/components/Search";
@@ -11,9 +13,63 @@ import DetailsContainer from "../../shared/components/DetailsContainer";
 import { PostInterface } from "../../types";
 import useTalentsReducer from "./hooks/useTalentsReducer";
 
+const ObsDiv = styled.div`
+  outline: 1px solid blue;
+  padding: 20px;
+`;
+
 const TalentsScreen: React.FC = function () {
-  const [state] = useTalentsReducer();
+  const [state, actions] = useTalentsReducer();
+  const { updateTalents } = actions;
   const active = Boolean(state.activeTalent);
+  const pageRef = React.useRef<number>(1);
+  const prevY = React.useRef<number>(0);
+
+  const [element, setElement] = React.useState<HTMLDivElement | null>(null);
+  const [loading, setLoading] = React.useState<boolean>(false);
+
+  const observer = React.useRef(
+    new IntersectionObserver(
+      (entries: IntersectionObserverEntry[]) => {
+        const { y } = entries[0].boundingClientRect;
+        if (prevY.current > y) {
+          setLoading(true);
+          setTimeout(() => {
+            handleLoadMore();
+          }, 500);
+        }
+        prevY.current = y;
+      },
+      { threshold: 1.0 },
+    ),
+  );
+
+  const fetchTalents = (page: number): void => {
+    axios.get<PostInterface[]>(`${process.env.REACT_APP_API}/talents/${page}`).then((res) => {
+      setLoading(false);
+      updateTalents(res.data);
+    });
+  };
+
+  const handleLoadMore = () => {
+    const nextPage = ++pageRef.current;
+    fetchTalents(nextPage);
+  };
+
+  React.useEffect(() => {
+    fetchTalents(pageRef.current);
+  }, []);
+
+  React.useEffect(() => {
+    if (element && state.more) {
+      loading ? observer.current.unobserve(element) : observer.current.observe(element);
+    }
+    return () => {
+      if (element) {
+        observer.current.unobserve(element);
+      }
+    };
+  }, [state.more, loading, element]);
 
   return (
     <Main active={active}>
@@ -31,6 +87,7 @@ const TalentsScreen: React.FC = function () {
             }}
           />
         ))}
+        <ObsDiv ref={setElement}>{loading && "Loading ..."}</ObsDiv>
       </PostsContainer>
       <DetailsContainer active={active}>
         {state.activeTalent ? <PostDetail data={state.activeTalent} /> : <PostDetailPlaceholder />}
