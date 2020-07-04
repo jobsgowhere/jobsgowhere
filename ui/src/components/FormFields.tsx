7cffea991a393bcf05bb54c5d2e808c0cb1b4b7c
Sheldon Cheng <sheldonscyd@gmail.com>
2020-07-05 01:37:35 +0800

    Add Radio component

--- ui/src/components/FormFields.tsx
+++ ui/src/components/FormFields.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import styled, { css } from "styled-components";
 
 const inputLayout = css`
@@ -53,3 +54,64 @@ export const Hint = styled.small`
   color: var(--color-grey-300);
   margin-bottom: 1rem;
 `;
+
+const StyledRadioGroup = styled.label`
+  display: flex;
+  input[type="radio"] {
+    position: absolute;
+    opacity: 0;
+    width: 0;
+    height: 0;
+    clip: rect(0, 0, 0, 0);
+  }
+`;
+
+const StyledRadio = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+  cursor: pointer;
+  padding: 0.125rem 0 0.125rem 2rem;
+  line-height: 1.275;
+
+  &::before {
+    border-radius: 100%;
+    content: "";
+    width: 1.5rem;
+    height: 1.5rem;
+    background-color: var(--color-grey-100);
+    margin-right: 0.5rem;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  &::after {
+    content: "";
+    width: 0.75rem;
+    height: 0.75rem;
+    background-color: var(--color-blue);
+    border-radius: 100%;
+    position: absolute;
+    left: 0.375rem;
+    top: 0.375rem;
+    visibility: hidden;
+  }
+
+  input:checked + &::after {
+    visibility: visible;
+  }
+`;
+
+type RadioProps = {
+  value: string;
+  name: string;
+  defaultChecked?: boolean;
+};
+
+export const Radio: React.FC<RadioProps> = ({ children, ...props }) => (
+  <StyledRadioGroup>
+    <input type="radio" {...props} />
+    <StyledRadio>{children}</StyledRadio>
+  </StyledRadioGroup>
+);
