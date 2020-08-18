b5ceb630e5769b70508b490129f86478ce2adc6d
Zahidur Rahman Faisal <d.m.zahidur.rahman@ihis.com.sg>
2020-08-19 07:33:59 +0800

    code: Added Firebase Analytics for user navigations
    - Click: New Post
    - Click: Favorites
    - Click: Profile
    - Event: Login
    - Event: Signup
    - Event: Logout

--- ui/src/components/Header/UserNav.tsx
+++ ui/src/components/Header/UserNav.tsx
@@ -4,34 +4,47 @@ import { Link } from "react-router-dom";
 import Auth0Context from "../../contexts/Auth0";
 import Button from "../Button";
 
+// GTM
+import * as GTMUtil from "../../shared/utils/GTMUtil";
+import useGTM from "@elgorditosalsero/react-gtm-hook";
+
+// Firebase
+import * as firebase from "firebase/app";
+import "firebase/analytics";
+
 const UserNav: React.FC = function () {
   const auth0Context = useContext(Auth0Context);
   const isAuthenticated = auth0Context?.state.matches("authenticated") ?? false;
+
+  const { sendDataToGTM } = useGTM();
 
   console.log(auth0Context?.state.value);
 
   const handleLogin = () => {
     auth0Context?.send("LOGIN");
+    firebase.analytics().logEvent("Event - LOGIN");
   };
   const handleSignup = () => {
     auth0Context?.send("SIGNUP");
+    firebase.analytics().logEvent("Event - SIGNUP");
   };
   const handleLogout = () => {
     auth0Context?.send("LOGOUT");
+    firebase.analytics().logEvent("Event - LOGOUT");
   };
 
   if (isAuthenticated) {
     return (
       <ul>
-        <li>
+        <li onClick={() => firebase.analytics().logEvent("Click - New Post")}>
           <Link to="/posts/new">
             <Button secondary>New Post</Button>
           </Link>
         </li>
-        <li>
+        <li onClick={() => firebase.analytics().logEvent("Click - Favourites")}>
           <Link to="/favourites">Favourites</Link>
         </li>
-        <li>
+        <li onClick={() => firebase.analytics().logEvent("Click - Profile")}>
           <Link to="/profile">Profile</Link>
         </li>
         <li>
