550360f790628498407998d4e36579c4110bd80f
Sheldon Cheng <sheldonscyd@gmail.com>
2020-10-15 00:08:47 +0800

    Implement new profile menu into UserNav

--- ui/src/components/Header/UserNav.tsx
+++ ui/src/components/Header/UserNav.tsx
@@ -1,12 +1,55 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
+import { Menu, StyledMenuItem, StyledMenuList } from "../../components/Menu";
 import Auth0Context from "../../contexts/Auth0";
+import { useProfile } from "../../contexts/Profile";
 import Button from "../Button";
+
+const ProfileImage = styled.img`
+  border-radius: 100%;
+  height: 4rem;
+  width: 4rem;
+`;
+
+const ProfileIcon = () => (
+  <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M20 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2M12 11a4 4 0 1 0 0-8 4 4 0 0 0 0 8z"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+const MyPostsIcon = () => (
+  <svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M19 13a2 2 0 0 1-2 2H5l-4 4V3a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v10z"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+const LogoutIcon = () => (
+  <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4M16 17l5-5-5-5M21 12H9"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
 
 const UserNav: React.FC = function () {
   const auth0Context = useContext(Auth0Context);
   const isAuthenticated = auth0Context?.state.matches("authenticated") ?? false;
+  const { profile } = useProfile();
 
   console.log(auth0Context?.state.value);
 
@@ -29,9 +72,22 @@ const UserNav: React.FC = function () {
           <Link to="/profile">Profile</Link>
         </li>
         <li>
-          <a onClick={handleLogout}>
-            <Button primary>Sign Out</Button>
-          </a>
+          <Menu button={<ProfileImage src={profile?.picture ?? ""} height="64" width="64" />}>
+            <StyledMenuList>
+              <StyledMenuItem as={Link} to="/profile">
+                <ProfileIcon />
+                Profile
+              </StyledMenuItem>
+              <StyledMenuItem as={Link} to="/my-posts">
+                <MyPostsIcon />
+                My Posts
+              </StyledMenuItem>
+              <StyledMenuItem onClick={handleLogout}>
+                <LogoutIcon />
+                Log Out
+              </StyledMenuItem>
+            </StyledMenuList>
+          </Menu>
         </li>
       </ul>
     );
