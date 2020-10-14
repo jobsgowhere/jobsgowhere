aff02f3e3f3270719be9a21ab57b522bf3ebcef8
Sheldon Cheng <sheldonscyd@gmail.com>
2020-10-15 00:07:46 +0800

    Revamp Menu component to use @react/menu-button

--- ui/src/components/Menu.tsx
+++ ui/src/components/Menu.tsx
@@ -1,9 +1,7 @@
-import React from "react";
+import { Menu as ReachMenu, MenuButton as ReachMenuButton, MenuPopover } from "@reach/menu-button";
+import { positionRight } from "@reach/popover";
+import * as React from "react";
 import styled from "styled-components";
-
-const Container = styled.div`
-  position: relative;
-`;
 
 export const StyledMenuList = styled.div`
   max-width: 12.5rem;
@@ -11,18 +9,23 @@ export const StyledMenuList = styled.div`
   border-radius: 0.875rem;
   padding: 1rem;
   box-shadow: 0 0 1rem rgba(163, 177, 198, 0.25);
-  position: absolute;
-  right: 0;
+  position: relative;
+  z-index: 3;
 `;
 
-type ButtonProps = {
-  onClick?: (e: React.MouseEvent) => void;
-};
-
-export const StyledMenuItem = styled.div`
+export const StyledMenuItem = styled.button`
+  background: transparent;
+  border: none;
   display: flex;
+  width: 100%;
+  cursor: pointer;
   align-items: center;
   white-space: nowrap;
+  font-size: 1rem;
+  padding: 0;
+  line-height: 1.375;
+  stroke: var(--color-darkblue);
+  color: var(--color-darkblue);
 
   & + & {
     margin-top: 0.75rem;
@@ -31,9 +34,14 @@ export const StyledMenuItem = styled.div`
   svg {
     margin-right: 0.5rem;
   }
+
+  &:hover {
+    color: var(--color-blue);
+    stroke: var(--color-blue);
+  }
 `;
 
-const StyledButton = styled.button`
+const MenuButton = styled(ReachMenuButton)`
   background: transparent;
   border: none;
   cursor: pointer;
@@ -43,44 +51,28 @@ const StyledButton = styled.button`
   }
 `;
 
-const OverflowMenuButton = React.forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => (
-  <StyledButton ref={ref} onClick={props.onClick}>
-    <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M12 13a1 1 0 100-2 1 1 0 000 2zM12 6a1 1 0 100-2 1 1 0 000 2zM12 20a1 1 0 100-2 1 1 0 000 2z"
-        stroke="#23374D"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  </StyledButton>
-));
+const OverflowIcon = (
+  <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M12 13a1 1 0 100-2 1 1 0 000 2zM12 6a1 1 0 100-2 1 1 0 000 2zM12 20a1 1 0 100-2 1 1 0 000 2z"
+      stroke="#23374D"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
 
-export const Menu: React.FC = ({ children }) => {
-  const [active, setActive] = React.useState(false);
-  const btnRef = React.useRef(null);
+type Props = {
+  button?: React.ReactNode;
+};
 
-  const handleWindowClick = (e: MouseEvent) => {
-    if ((e.target as HTMLButtonElement).closest("button") !== btnRef.current) setActive(false);
-  };
-
-  React.useEffect(() => {
-    window.addEventListener("click", handleWindowClick);
-    return () => {
-      window.removeEventListener("click", handleWindowClick);
-    };
-  }, []);
-
-  const handleClick = (e: React.MouseEvent) => {
-    e.preventDefault();
-    if (e.currentTarget === btnRef.current) setActive((val) => !val);
-  };
-
+export const Menu: React.FC<Props> = ({ button, children }) => {
+  const ButtonInner = button || OverflowIcon;
   return (
-    <Container>
-      <OverflowMenuButton onClick={handleClick} ref={btnRef} />
-      {active && React.Children.only(children)}
-    </Container>
+    <ReachMenu>
+      <MenuButton>{ButtonInner}</MenuButton>
+      <MenuPopover position={positionRight}>{children}</MenuPopover>
+    </ReachMenu>
   );
 };
