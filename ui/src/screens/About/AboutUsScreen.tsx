39609bdb9ec33e75a17a760c9d3f20d901c6beea
Sheldon Cheng <sheldonscyd@gmail.com>
2020-11-03 21:48:24 +0800

    New MainSingleLarge component. Update AboutUsScreen

--- ui/src/screens/About/AboutUsScreen.tsx
+++ ui/src/screens/About/AboutUsScreen.tsx
@@ -1,45 +1,32 @@
 import * as React from "react";
 import styled from "styled-components";
 
-const MainSingle = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  grid-area: main;
-  max-width: 735px; 
-  width: 100%;
-  margin: 0 auto;
-  font-family: 'Nunito';
-  font-size: 18px;
-  font-weight: 400;
-`;
-
-const Title = styled.h1`
-  font-size: 1.5rem;
-  text-align: center;
-`;
+import { MainSingleLarge } from "../../components/Main";
 
 const Container = styled.div`
   background: #fff;
   border-radius: 0.875rem;
-  align-self: stretch;
-  padding: 1rem;
+  padding: 2rem;
+  color: var(--color-grey-300);
 `;
 
 const AboutUsScreen: React.FC = function () {
   return (
-    <MainSingle>
-      <Title>The JobsGoWhere Story</Title>
-      <Container> 
-        Hello. Welcome to JobsGoWhere.
-        <p />
-        We are a group of individuals working on this platform on our free time to bridge the gap between job seekers and
-        job providers. We hope this platform is able to help people during this time of crisis where many may be unfortunately displaced.
-        <p />
-        If there are any enquiries, please drop the team a message <a href="mailto:jgw@jgw.com">here</a>.
-
+    <MainSingleLarge>
+      <h1>The JobsGoWhere Story</h1>
+      <Container>
+        <p>Hello. Welcome to JobsGoWhere.</p>
+        <p>
+          We are a group of individuals working on this platform on our free time to bridge the gap
+          between job seekers and job providers. We hope this platform is able to help people during
+          this time of crisis where many may be unfortunately displaced.
+        </p>
+        <p>
+          If there are any enquiries, please drop the team a message{" "}
+          <a href="mailto:jgw@jgw.com">here</a>.
+        </p>
       </Container>
-    </MainSingle>
+    </MainSingleLarge>
   );
 };
 
