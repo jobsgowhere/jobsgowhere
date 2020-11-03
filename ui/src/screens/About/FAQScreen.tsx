1f35c1593c3f23731e11d8a6045ec5457e7cba02
Sheldon Cheng <sheldonscyd@gmail.com>
2020-11-03 23:00:20 +0800

    Simplify accordion toggle logic

--- ui/src/screens/About/FAQScreen.tsx
+++ ui/src/screens/About/FAQScreen.tsx
@@ -47,55 +47,55 @@ const Internal = styled.div<{collapsed: boolean}>`
 const contents = [
   {
     question: "How do I connect with a Job Provider?",
-    answer: "Click the Connect with button on the jobs board to launch a message screen. Then, type in your greetings and \
-    perhaps a pitch, then send the message. The recruiter will receive a notification by email about your interest."
+    answer:
+      "Click the Connect with button on the jobs board to launch a message screen. Then, type in your greetings and \
+    perhaps a pitch, then send the message. The recruiter will receive a notification by email about your interest.",
+    collapsed: true,
   },
   {
     question: "How do I connect with a Job Seeker?",
-    answer: "Click the Connect with button on the talents board to launch a message screen. Then, send some useful information \
-    about the job like a Job Description or a Company Website. The talent will receive your message via email."
+    answer:
+      "Click the Connect with button on the talents board to launch a message screen. Then, send some useful information \
+    about the job like a Job Description or a Company Website. The talent will receive your message via email.",
+    collapsed: true,
   },
   {
     question: "Is this platform for tech only?",
-    answer: "No! This platform is for anyone who is seeking a job or hiring!"
+    answer: "No! This platform is for anyone who is seeking a job or hiring!",
+    collapsed: true,
   },
   {
     question: "Am I guaranteed a job on this platform?",
-    answer: "We hope everyone is able to find a match here, but we can offer no guarantees. If you are seeking a job, make yourself more \
+    answer:
+      "We hope everyone is able to find a match here, but we can offer no guarantees. If you are seeking a job, make yourself more \
     attractive by including links to your resume and accomplishments in your message. If you are looking to hire, be clear with the job \
-    requirement, what you are looking for, and compensation provided. No one likes a shady deal!"
-  }
-]
+    requirement, what you are looking for, and compensation provided. No one likes a shady deal!",
+    collapsed: true,
+  },
+];
 
 const FAQScreen: React.FC = function () {
+  const [faq, setFaq] = React.useState(contents);
 
-  let collapsedInitial = new Array(contents.length);
-  collapsedInitial.fill(true);
-  const [collapsed, setCollapsed] = React.useState(collapsedInitial);
-
-  const toggleAnswer = function(event: React.MouseEvent) : void {
-    const id = parseInt(event.currentTarget.id);
-    let newCollapsed = [...collapsed];
-    newCollapsed[id] = !collapsed[id];
-    setCollapsed(newCollapsed);
-  }
+  const toggleAnswer = (index: number) => {
+    faq[index].collapsed = !faq[index].collapsed;
+    setFaq([...faq]);
+  };
 
   return (
     <MainSingle>
       <Title>Frequently Asked Questions</Title>
-      {contents.map((content, index) => {
+      {faq.map((content, index) => {
+        const { question, answer, collapsed } = content;
         return (
-        <Accordion id={index.toString()} onClick={toggleAnswer}> 
-          <AccordionHeader collapsed={collapsed[index]}>
-            {content.question}
-          </AccordionHeader>
-          <Internal collapsed={collapsed[index]}>
-            <br />
-            {content.answer}
-          </Internal>
-        </Accordion>);
-      }) }
-      
+          <Accordion key={question}>
+            <AccordionHeader collapsed={collapsed} onClick={() => toggleAnswer(index)}>
+              {question}
+            </AccordionHeader>
+            <Internal collapsed={collapsed}>{answer}</Internal>
+          </Accordion>
+        );
+      })}
     </MainSingle>
   );
 };
