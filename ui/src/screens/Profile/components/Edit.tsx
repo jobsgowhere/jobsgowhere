0017b3e1b6b824fc628399cd895bb8fd1c79d403
Jacob Tan <jacobtyq@gmail.com>
2020-11-13 18:36:45 +0800

    Disable email field from editing

--- ui/src/screens/Profile/components/Edit.tsx
+++ ui/src/screens/Profile/components/Edit.tsx
@@ -232,6 +232,7 @@ const Edit: React.FC<ProfileEditProps> = ({ profile, newUser, handleCancelEdit }
           id="email"
           name="email"
           defaultValue={email}
+          disabled
           ref={register({
             required: true,
             pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
